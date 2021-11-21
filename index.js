aa133a6d042c1bf639326fa3906d574a7ef61413
Dhinesh-Dhinesh <dhineshrandy953@gmail.com>
2021-11-21 10:33:47 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,96 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+require('./employee');
+//!<----------------------------------->
+
+app.use(bodyParser.json());
+
+
+const mongoUri = 'mongodb+srv://dhi:44FR83yNbBkuHdZc@cluster0.kkndm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const Employee = mongoose.model('employee');
+
+//--------------- FOR CONNECTION
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database');
 })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Example app listening on port 3000!');
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+})
+
+//---------------//
+app.get('/', (req, res) => {
+    res.send('Hello bro');
+})
+
+app.get('/ok', (req, res) => {
+    Employee.find({}).then((employees) => {
+        res.send(employees);
+    }).catch((err) => {
+        console.log(err);
+    })
+})
+
+//FOR SEND
+
+app.post('/send', (req, res) => {
+    const employee = new Employee({
+        name:req.body.name,
+        email:req.body.email,
+        phone:req.body.phone,
+        salary:req.body.salary,
+        position:req.body.position
+    });
+
+    employee.save().then(data => {                                 //then catch
+        console.log(data);
+        res.send('Data received');
+    }).catch(err => {
+        console.log(err);
+    })
+})
+
+//FOR DELETE
+
+app.post('/delete',(req,res)=>{
+    Employee.findByIdAndRemove(req.body.id).then(data=>{
+        console.log(data);
+        console.log('Data deleted');
+        res.send('Data deleted');
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
+//FOR UPDATE
+
+app.post('/update',(req,res)=>{
+    Employee.findByIdAndUpdate(req.body.id,{
+        name:req.body.name,
+        email:req.body.email,
+        phone:req.body.phone,
+        salary:req.body.salary,
+        position:req.body.position
+    }).then(data=>{                         
+        console.log(data);
+        res.send('Data updated');
+        console.log('Data updated');    
+    }).catch(err=>{
+        console.log(err);
+    })
+})
+
+//PORT
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log('Server started at port 3000');
 })
